672b2a1821c875d36b4ea28fb3791ce5cbec5a39
Andrew Vos <andrew.vos@gmail.com>
2012-01-29 18:06:49 +0000

    Added vimfiles from github

--- manifests/ubuntu-1104-server-amd64-with-puppet-support.pp
+++ manifests/ubuntu-1104-server-amd64-with-puppet-support.pp
@@ -2,20 +2,54 @@ group { "puppet":
   ensure => "present",
 }
 
+class basenode {
+  include motd
+  include git
+  include tmux
+  include vim
+}
+include basenode
+
 File { owner => 0, group => 0, mode => 0644 }
 
-file { '/etc/motd':
-  content => "Welcome to Vos's Dev Box"
+class motd {
+  file { '/etc/motd':
+    content => "Welcome to Vos's Dev Box"
+  }
 }
 
-package{ "vim-nox":
-  ensure => "present"
+class git {
+  package{ "git":
+    ensure => present
+  }
 }
 
-package{ "git":
-  ensure => "present"
+class tmux {
+  package{ "tmux":
+    ensure => present
+  }
 }
 
-package{ "tmux":
-  ensure => "present"
+class vim {
+  package{ "vim-nox":
+    ensure => present
+  }
+
+  exec{ "vimrc":
+    command => "/usr/bin/curl -s -o /home/vagrant/.vimrc https://raw.github.com/AndrewVos/vimfiles/master/.vimrc"
+  }
+
+  exec{ "vundle":
+    command => "/usr/bin/git clone http://github.com/gmarik/vundle.git /home/vagrant/.vim/bundle/vundle",
+    creates => "/home/vagrant/.vim/bundle/vundle/README.md",
+    require => Package["git"]
+  }
+
+  file{ "/home/vagrant/.vim":
+    recurse => true,
+    owner => "vagrant",
+    group => "vagrant",
+    mode => 644,
+    require => Exec["vundle"]
+  }
 }
