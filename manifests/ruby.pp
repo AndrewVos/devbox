f7349296fcbb7a86ee6ca6c372758d450d8dc40d
Andrew Vos <andrew.vos@gmail.com>
2012-02-05 16:27:12 +0000

    Install ruby 1.9.3 and set it as the default ruby

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,9 +1,18 @@
 class ruby {
   exec { "rvm":
     command => "/bin/su -l vagrant -c '/bin/bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)'",
-    require => [
-      Package["git"]
-    ],
+    require => Package["git"],
     creates => "/home/vagrant/.rvm/bin/rvm"
+  }
+
+  exec { "ruby-1.9.3":
+    command => "/bin/su -l vagrant -c 'rvm install 1.9.3'",
+    require => Exec["rvm"],
+    creates => "/home/vagrant/.rvm/rubies/ruby-1.9.3-p0/bin/ruby"
+  }
+
+  exec {"default-ruby":
+    command => "/bin/su -l vagrant -c 'rvm use --default 1.9.3'",
+    require => Exec["ruby-1.9.3"],
   }
 }
